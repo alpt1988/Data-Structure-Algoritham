e8c239dc88cb55d787b021efdf2b420eeeb2cf7a
alpt1988 <alpt1988@gmail.com>
2022-01-31 13:30:20 -0500

    implemented queue, stack and hastable data structure

---
+++ Data Structure/queue.java
@@ -0,0 +1,50 @@
+// Linear data structure
+// flexible with size, don't have to allocate initially
+// add(add last) remove(remove first)
+public class queue {
+    private static class Node{
+        private int data;
+        private Node next;
+        private Node(int data){
+            this.data = data;
+        }
+    }
+
+    private Node head;//remove from the head
+    private Node tail;// add thing to the tail
+
+    // if head is empty reutrn null, return true
+    public boolean isEmpty(){
+        return head == null;
+    };
+
+    //reutrn the head data
+    public int peek(){
+        return head.data;
+    };
+
+    // add to the last, create a new node and let the old tail point to this node. let the new node be the tail
+    // if the queue is completely empty, then this new node should be head
+    public void add(int data){
+        Node node = new Node(data);
+        if (tail != null){
+            tail.next = node;
+        }
+        tail = node;
+        if (head == null){
+            head = node;
+        }
+    };
+
+    // first get the head data
+    // remove the head
+    // if head is null then tail is null too and return the data
+    public int remove(){
+        int data = head.data;
+        head = head.next;
+        if (head == null){
+            tail = null;
+        }
+        return data;
+    };
+}
---
+++ Data Structure/stack.java
@@ -0,0 +1,41 @@
+// Linear data structure
+// flexible with size, don't have to allocate initially
+// push(add first) pop(remove first)
+public class stack {
+    private static class Node{
+        private int data;
+        private Node next;
+        private Node(int data){
+            this.data = data;
+        }
+    }
+
+    private Node top;// add and remove from the top
+
+    // if top is null, return true
+    public boolean isEmpty(){
+        return top == null;
+    };
+
+    public int peek(){
+        return top.data;
+    };
+
+    // creat a new node
+    // then point to the old head
+    // top will be the new top
+    public void push(int data){
+        Node node = new Node(data);
+        node.next = top;
+        top = node;
+    };
+
+    // get the top data
+    // top should be the next element down
+    // return the data
+    public int pop(){
+        int data = top.data;
+        top = top.next;
+        return data;
+    };
+}
