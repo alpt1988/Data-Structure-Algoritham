aff531dd14d427b0fd4fdbff22d63a036f835134
alpt1988 <alpt1988@gmail.com>
2022-02-22 14:31:22 -0500

    implemented two tree problems

---
+++ CTCI /Tree/maximumDepthOfBinaryTree.java
@@ -0,0 +1,23 @@
+public class maximumDepthOfBinaryTree {
+    public class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(){};
+        TreeNode(int val){this.val = val;}
+        TreeNode(int val, TreeNode left, TreeNode right){
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+    public int maxDepth(TreeNode root){
+        if (root == null){
+            return 0;
+        }else {
+            return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+        }
+    }
+}
---
+++ CTCI /Tree/sameTree.java
@@ -0,0 +1,29 @@
+public class sameTree {
+    public class Treenode{
+        int val;
+        Treenode left;
+        Treenode right;
+        Treenode(){};
+        Treenode(int val){this.val = val;}
+        Treenode(int val, Treenode left, Treenode right){
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isSameTree(Treenode p. Treenode q){
+        if ( p == null && q == null){
+            return true;
+        }else if ( p == null || q == null || p.val != q.val){
+            return false;
+        }else {
+            return (isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
+        }
+    }
+
+
+
+
+
+}
